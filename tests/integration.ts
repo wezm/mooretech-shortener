3bbbedee2e9470c8746bd2ec9e16dc0b4183de83
Wesley Moore <wes@wezm.net>
2023-02-20 09:43:55 +1000

    Fix integration test

--- tests/integration.ts
+++ tests/integration.ts
@@ -1,11 +1,11 @@
-import { assertStrictEquals } from "https://deno.land/std@0.156.0/testing/asserts.ts";
+import { assertStringIncludes } from "https://deno.land/std@0.156.0/testing/asserts.ts";
 import "../src/index.ts";
 
 Deno.test({
   name: "Integration",
   fn: async () => {
     const response = await fetch("http://localhost:8000", { method: "GET", });
-    assertStrictEquals(await response.text(), "Hello from Rust 🦀");
+    assertStringIncludes(await response.text(), "This domain hosts a URL shortener");
   },
   sanitizeResources: false,
   sanitizeOps: false,
